d72d9f1dccb3a28d77c5616c6ba56e206e9f8feb
chrisrees222 <143870938+chrisrees222@users.noreply.github.com>
2024-07-07 10:47:04 +0100

    Add project files.

---
+++ Web-Portfolio-New-july-24/Pages/Skills.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_Portfolio_New_july_24.Pages
+{
+    public class SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
